58a21197bd33f46b4e7871583371a9a09cfe80c2
Michal <87991652+Mrorzonki@users.noreply.github.com>
2022-09-06 20:42:45 +0200

    chore[paramiter]: export type

--- get_artist_albums.py
+++ get_artist_albums.py
@@ -1,19 +1,32 @@
 #module imports
 import credentials as cred
 import spotipy_module as spm
-import json #TODO: remove - test only
 
 #other imports
 import sys
+import json
 
-def main(artist_name='Casting Crowns', export_type='raw', path_name='C:/Users/X/Desktop/out'):
+def main(artist_name='Casting Crowns', export_type='raw', export_pathname='C:/Users/X/Desktop/out'):
     auth_sp = spm.get_authentication(cred.client_id, cred.client_secret) 
     
     artist = spm.get_artist_by_name(auth_sp, artist_name)
     albums = spm.get_albums_by_artist(auth_sp, artist['id'])
 
-    print(json.dumps(albums))
+    if(export_type=='csv'): print('Export mode = csv')
+    elif(export_type=='excel'): print('Export mode = excel')
+    elif(export_type=='json'): print('Export mode = json')
+    else: print('Export mode = raw')
 
 if __name__ == '__main__':
     argv = sys.argv
-    main()
+
+    #TODO: parmeters validation
+    if(len(argv) < 2 or 3 < len(argv)):  sys.exit("ERROR: invalid parameters count") #script requires 2 to 3 parameters
+       
+    artist_name=argv[1]
+    export_type=argv[2].lower()
+
+    if(export_type not in ['csv','excel','json','raw']): sys.exit("ERROR: invalid export type selected") #avaliable eksport types
+    
+    #export_pathname=argv[3] #TODO: provide devault export pathname 
+    main(export_type=export_type) #TODO
