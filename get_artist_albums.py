6b5e2968357243814f3d283903e5c08691fa89a4
Michal <87991652+Mrorzonki@users.noreply.github.com>
2022-09-10 21:26:11 +0200

    refactor[main]: move error messages out of code

--- get_artist_albums.py
+++ get_artist_albums.py
@@ -6,6 +6,16 @@ import export_module as exm
 #other imports
 import sys
 
+#messages
+error_pathname_missing = "SCRIPT ERROR: export_pathname missing."
+error_type_missing = "SCRIPT ERROR: export_type missing."
+
+error_invalid_paramiters = ("Error: invalid paramiters provided"
++ "\nTry: get_artist_albums.py 'artist name' 'export type' 'file pathname'"
++ "\n  'artist name' - name of the aritst in spotify"
++ "\n  'export type' - avalible output formats: 'csv','excel','json' (each crates a file) or 'raw' (wites to console)"
++ "\n  'file pathname' - optional pathname for output file (without file extension), when skipped artist_name is used")
+
 def main(artist_name, export_type, export_pathname=None):
     export_type = export_type.lower()
 
@@ -16,13 +26,13 @@ def main(artist_name, export_type, export_pathname=None):
 
     if(export_type=='raw'): exm.write_to_raw(json_data=albums)
     else:
-        if(export_pathname == None): sys.exit("SCRIPT ERROR: export_pathname missing.") #backup check if pathname provided, if no script requires fix  
+        if(export_pathname == None): sys.exit(error_pathname_missing) #backup check if pathname provided, if no script failed and requires fix 
 
         #if file allready exists, below will exit with error message, set overwrite to True to disable
         if(export_type=='csv'): exm.write_to_csv(json_data=albums,pathname=export_pathname+'.csv')
         elif(export_type=='excel'): exm.write_to_excel(json_data=albums,pathname=export_pathname+'.xlsx')
         elif(export_type=='json'): exm.write_to_jason(json_data=albums,pathname=export_pathname+'.json')
-        else: sys.exit("SCRIPT ERROR: export_type missing.") #if execution reached this line script requires fix  
+        else: sys.exit(error_type_missing) #if execution reached this line script failed and requires fix
     
 if __name__ == '__main__':
     argv = sys.argv
@@ -37,11 +47,6 @@ if __name__ == '__main__':
             if(len(argv) == 4): export_pathname = exm.rmv_file_extension(argv[3]) #check if provided output file pathname, if yes clear its file extension... 
             else: export_pathname = exm.get_snake_case(artist_name) #... if not create pathname of artist_name
             main(artist_name=artist_name, export_type=export_type, export_pathname=export_pathname)
-            
+        
     else:
-        error = "Error: invalid paramiters provided"
-        error += "\nTry: get_artist_albums.py 'artist name' 'export type' 'file pathname'"
-        error += "\n  'artist name' - name of the aritst in spotify"
-        error += "\n  'export type' - avalible output formats: 'csv','excel','json' (each crates a file) or 'raw' (wites to console)"
-        error += "\n  'file pathname' - optional pathname for output file (without file extension), when skipped artist_name is used"
-        sys.exit(error)
+        sys.exit(error_invalid_paramiters)
